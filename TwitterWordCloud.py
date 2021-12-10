0e059ddc60c4749e818003c7d800650caae253fa
Saleh Saeed <45196857+sa92leh-fs@users.noreply.github.com>
2021-12-11 01:39:20 +0300

    Update TwitterWordCloud.py

--- TwitterWordCloud.py
+++ TwitterWordCloud.py
@@ -96,6 +96,4 @@ def submit():
     return render_template("Twitter.html",image = full_filename,Hashtag=Hashtag,Tweets=Tweets)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
     app.run(debug=True)
